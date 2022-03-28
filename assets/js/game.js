c69353bd97f5a55bca3a6c2ae261bfbaa26e3b5a
Jason Libertelli <jason.libertelli@gmail.com>
2022-03-28 17:28:07 -0400

    highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,11 +169,22 @@ var fight = function(enemy) {
     };
 
     var endGame = function() {
-        //if player is still alive, player wins! 
-        if (playerInfo.health > 0) {
-            window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-        } else { 
-            window.alert("You've lost your robot in battle.");
+        window.alert("The game has now ended. Let's see how you did!");
+
+        //check localstorage for high score, if it's not there, use 0
+        var highScore = localStorage.getItem("highscore");
+        if(highScore === null){
+            highScore = 0;
+        }
+        //if player has more money than the highscore, player has new highscore.
+        if(playerInfo.money > highScore){
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        }
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
         }
 
         var playAgainConfirm = window.confirm("Would you like to play again?")
